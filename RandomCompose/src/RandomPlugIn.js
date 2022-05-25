7dccb58da7d43fe0b808f683d7efaf577ad91120
Ruichen Liu <102811722+bjRichardLiu@users.noreply.github.com>
2022-05-25 15:22:54 -0400

    Update and rename PluginTest.js to RandomPlugIn.js

--- RandomCompose/src/RandomPlugIn.js
+++ RandomCompose/src/RandomPlugIn.js
@@ -1,10 +1,14 @@
+//Handles the midi event
+
 function HandleMIDI(event) {
 
     var randomNote;
 
+    // Use the pitch and velocity of the note for generating the new note
     var a = event.pitch % 11 + 1;
     var b = event.velocity % 13 + 2;
 
+    //Fake random to make sure the note generated each time is the same
     var c = (a + b) * ((a + 1) % (b + 3) + 1);
 
     while (c < 48 || c > 83) {
@@ -18,6 +22,8 @@ function HandleMIDI(event) {
     randomNote = c;
 
     event.pitch = randomNote;
+    
+    //sets the velocity to 80
     event.velocity = 80;
 
     event.send();
